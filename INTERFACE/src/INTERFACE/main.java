5ace6d388ea50f6419925d9d36dbda90003ad57b
eliaras <apoel_881@hotmail.com>
2014-05-28 11:27:04 +0300

    new changes

--- INTERFACE/src/INTERFACE/main.java
+++ INTERFACE/src/INTERFACE/main.java
@@ -3,26 +3,29 @@ package INTERFACE;
 import java.sql.*;
 
 public class main {	 
-	static final String JDBCDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	static final String connectionStr =
-					"jdbc:sqlserver://Data Source=VAIO;Initial Catalog=Clubs;Integrated Security=True";
+
+	//static final String JDBCDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+//	static final String connectionStr =
+					//"jdbc:sqlserver://Data Source=VAIO;Initial Catalog=Clubs;Integrated Security=True";
+					
+			//		static final String connectionStr =
+				//			"jdbc:sqlserver://localhost;database=VAIO;Initial Integrated Security=True";		
 	 
 	public static void main(String[] args)  {
 		
 		 try{
 			 
 			 
-			 String userName ="username";
-			 String password ="password";
+			// String userName ="username";
+			// String password ="password";
 			 
+			// String url = "jdbc:sqlserver://MYPC\\SQLEXPRESS;databaseName=MyTest;integratedSecurity=true";
+			 String url = "jdbc:sqlserver://VAIO\\MSSQLSERVER; databaseName=MyTest; integratedSecurity=true";
 			 
 			 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			 Connection conn = DriverManager.getConnection(connectionStr,userName,password);
+			 Connection conn = DriverManager.getConnection(url);
 			 
-			 
-			
-		
-			   
+			  
 			  
 				if (conn != null) 
 				    System.out.println("Connected");					
